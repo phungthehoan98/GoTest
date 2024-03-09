998f07a384dc6edc3ace1400889ac98f8da9964a
Hoan Phung <https://HoanPT@bitbucket.org>
2024-03-09 22:19:40 +0700

    day 4

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Go Test for the new language
  
 
  
-Start: 
+# Start: 
     export SNIPPETBOX_ADDR=":9999"
     go run cmd/web/* -addr=$SNIPPETBOX_ADDR
-Note: 
+# Note: 
     9999 is your port want to use (http://localhost:9999/)
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -43,6 +43,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
+	expires := "7"
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	// curl -iL -X POST http://localhost:4000/snippet/create
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
